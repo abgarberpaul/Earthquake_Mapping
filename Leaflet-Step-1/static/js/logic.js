fc53a184ebedb6ba0ef256aa27d6abb91391e260
Aaron Garber-Paul <aaron@MacBook-Pro-2.local>
2021-02-02 13:42:42 -0800

    barebones - working base layer and pins

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -19,9 +19,75 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson";
 
 
+function earthQuakeColor (mag){
+  if (mag>90)
+  return "purple"
+  if (mag>70)
+  return "red"
+  else if (mag>50)
+  return "orange"
+  else if (mag>30)
+  return "yellow"
+  else if (mag>10)
+  return "white"
+  else 
+  return "green"
+}
+
 // Grab the data with d3
 d3.json(link).then(function(data) {
   console.log(data);
+
+  // Build flags
+  L.geoJson(data, {
+    onEachFeature: function(feature, layer){
+      layer.bindPopup(
+        feature.properties.place+"<hr> Magnitude: "+
+        feature.properties.mag
+
+
+      )
+    },
+  
+  style: function(feature){
+    return {
+    color : "black",
+    radius : feature.properties.mag*4,
+    fillOpacity : 0.5,
+    fillColor : earthQuakeColor(feature.geometry.coordinates[2])
+  }
+  },
+
+
+  pointToLayer: function(feature, latlng){
+    return L.circleMarker(latlng)
+  }
+
+  }).addTo(myMap)
+
+  // code for legend
+  
+  var legend = L.control({position: 'bottomright'});
+
+  legend.onAdd = function (map) {
+  
+      var div = L.DomUtil.create('div', 'info legend'),
+          magnitudes = [-10, 10, 30, 50, 70, 90],
+          labels = [];
+  
+      // loop through our density intervals and generate a label with a colored square for each interval
+      for (var i = 0; i < magnitudes.length; i++) {
+          div.innerHTML +=
+              '<i style="background:' + earthQuakeColor (magnitudes[i] + 1) + '"></i> ' +
+              magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
+      }
+  
+      return div;
+  };
+  
+  legend.addTo(myMap);
+
+
   // data.forEach(function(data) {
   //   var lat = data.features.geometry.coordinates;
   //   var lng = 
