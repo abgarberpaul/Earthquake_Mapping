fce600b94da029d08b13500f3e4ce3dd45ee564c
Aaron Garber-Paul <aaron@MacBook-Pro-2.local>
2021-02-03 01:05:59 -0800

    second half

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -2,24 +2,62 @@
 var Quake_link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson";
 var Tec_link = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
 
-// Create a map object
-var myMap = L.map("map", {
-  center: [21.3068, -157.7912],
-  zoom: 2.45
-});
+// // SET UP: define LayersGroups
 
-// Adding tile layer to the map
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var quakes = new L.LayerGroup();
+var plates = new L.LayerGroup();
+
+// // SET UP: define variables for layers
+
+var standardMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  });
+
+var grayMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
-  id: "mapbox/streets-v11",
+  id: "mapbox/light-v9",
   accessToken: API_KEY
-}).addTo(myMap); 
+});
 
+var geoMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/satellite-v9",
+  accessToken: API_KEY
+});
 
+// Create a base map object to hold base layer variables
+var baseMaps = {
+  "Standard" : standardMap,
+  "Gray": grayMap,
+  "Satellite" : geoMap
+};
 
+// Overlay Object (holds over layers)
+ var mapLayers = {
+   "Earthquakes": quakes,
+   "Tectonic Plate Lines": plates
+ }
+
+// Create Map, pass in standard layers for default setting
+var myMap = L.map("map", {
+  center: [21.3068, -157.7912],
+  zoom: 2.45,
+  layers: [standardMap, quakes]
+});
+
+// Create a Layer Control + Pass in baseMaps and overlayMaps + Add the Layer Control to the Map
+L.control.layers(baseMaps, mapLayers).addTo(myMap);
 
 // controls the colors of the dots (paired to MAG)
 function earthQuakeColor (mag){
@@ -39,7 +77,7 @@ function earthQuakeColor (mag){
   return "DarkGreen"
 }
 
-// Grab the data with d3
+// Grab the QUAKE data with d3
 d3.json(Quake_link).then(function(data) {
   console.log(data);
 
@@ -47,7 +85,7 @@ d3.json(Quake_link).then(function(data) {
   // (currently flags only appear when CLICKed)
   
   // code sets text on flag
-  // set to display MAG 
+  // set to display TIME, MAG, DEPTH
   L.geoJson(data, {
     onEachFeature: function(feature, layer){
       var myDate = new Date(feature.properties.time);
@@ -61,33 +99,33 @@ d3.json(Quake_link).then(function(data) {
   // set FLAG STYLE
   // radius to a multiple of MAG
   // color set to DEPTH
-  style: function(feature){
-    return {
-    color : earthQuakeColor(feature.geometry.coordinates[2]),
-    radius : Math.pow(feature.properties.mag, 2)/2,
-    fillOpacity : 0.75,
-    fillColor : earthQuakeColor(feature.geometry.coordinates[2])
-  }
+    style: function(feature){
+      return {
+      color : earthQuakeColor(feature.geometry.coordinates[2]),
+      radius : Math.pow(feature.properties.mag, 2)/2,
+      fillOpacity : 0.75,
+      fillColor : earthQuakeColor(feature.geometry.coordinates[2])
+    }
   },
-
 
   pointToLayer: function(feature, latlng){
     return L.circleMarker(latlng)
   }
 
-  }).addTo(myMap)
+  }).addTo(quakes);
+  quakes.addTo(myMap);
 
+//  // QUAKE layer
+// Create a variable for the new data set
+  var plateData
 
-  // Retrieve platesURL (Tectonic Plates GeoJSON Data) with D3
-  d3.json(Tec_link, function(Tec_Lines) {
-    // Create a GeoJSON Layer the Tec_Lines
-    L.geoJson(Tec_Lines, {
-        color: "#DC143C",
-        weight: 2
-    // Add plateData to tectonicPlates LayerGroups 
-    }).addTo(tectonicPlates);
-    // Add tectonicPlates Layer to the Map
-    tectonicPlates.addTo(myMap);
+  // Grab plate data with d3
+  d3.json(Tec_link).then(function(tec_data) {
+    console.log(tec_data);
+    L.geoJson(tec_data).addTo(plates);
+
+    // Add Layer to the Map
+    plates.addTo(myMap)
 });
 
 
